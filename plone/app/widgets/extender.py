98d90667d796eabc61ad7ae28fbe788f21d95554
Nathan Van Gheem <vangheem@gmail.com>
2011-11-12 23:37:33 -0600

    add ability to directly add js options

--- plone/app/widgets/extender.py
+++ plone/app/widgets/extender.py
@@ -1,16 +1,19 @@
 from plone.widgets.archetypes import ChosenWidget
 from archetypes.schemaextender.interfaces import ISchemaModifier
+from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
 from zope.component import adapts
 from zope.interface import implements
 from Products.ATContentTypes.interface import IATContentType
+from plone.app.widgets.interfaces import ILayer
 
 
 class WidgetsModifier(object):
     """
     change any content...
     """
+    implements(ISchemaModifier, IBrowserLayerAwareExtender)
     adapts(IATContentType)
-    implements(ISchemaModifier)
+    layer = ILayer
 
     def __init__(self, context):
         self.context = context
@@ -21,5 +24,9 @@ class WidgetsModifier(object):
             subject_widget = subject.widget
             subject.widget = ChosenWidget(
                 label=subject_widget.label,
-                description=subject_widget.description)
+                description=subject_widget.description,
+                js_options={
+                    'allow_add_new': True,
+                    'no_results_text': 'No results. Press enter to add'
+                })
             subject.vocabulary_factory = 'plone.app.vocabularies.Keywords'
