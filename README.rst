9d2fdf33a46eaa2dbc9c26d0cbdae0fc4e05d1b0
Rok Garbas <rok@garbas.si>
2012-12-17 18:43:24 +0100

    package organization changes
    - *.txt -> *.rst
    - merge description and long_description with plone.widgets package
    - drop dependency on plone.widgets
    - fill out the rest of the setup.py options (author, ...)

---
+++ README.rst
@@ -0,0 +1,40 @@
+``plone.app.widgets`` is a revamp of plone widgets. It does this by overriding
+the widgets of some of the fields in plone.
+
+
+Used Libraries
+==============
+
+ - Chosen
+ - Chosen with drag n drop sorting
+ - Chosen with ajax
+ - Chosen with date/time
+
+
+Fields overridden 
+=================
+
+ - tag widget with auto-complete, sortable Chosen widget
+ - contributors, creators with Chosen ajax widget
+ - collection field selector with sortable Chosen widget
+ - related items with ajax Chosen widget
+
+
+Roadmap
+=======
+
+0.5
+---
+
+All archetypes archetypes mult-select widgets overriden and date/time widgets
+
+0.9
+---
+
+z3cform widgets overridden
+
+1.0
+---
+
+formlib widgets done
+
--- setup.py
+++ setup.py
@@ -1,37 +1,38 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.1'
 
-setup(name='plone.app.widgets',
-      version=version,
-      description="integrate plone widgets",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.app.widgets',
+    version=version,
+    description="better plone widgets",
+    long_description='%s\n%s' % (
+        open("README.rst").read(),
+        open("CHANGES.rst").read(),
+    ),
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.widgets',
-          'archetypes.schemaextender'
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    ],
+    keywords='plone widgets z3cform archetypes',
+    author='Nathan Van Gheem',
+    author_email='vangheem@gmail.com',
+    url='https://github.com/plone/plone.app.widgets',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require={
+        'archetypes': [
+            'archetypes.schemaextender',
+        ]
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
