b8758f3fe32ebc73f2b6e15e23d60364257b037b
Rok Garbas <rok@garbas.si>
2013-01-09 19:41:31 +0100

    autocomplete view now supports query

--- plone/app/widgets/autocomplete.py
+++ plone/app/widgets/autocomplete.py
@@ -1,4 +1,5 @@
 import json
+import inspect
 from zope.component import queryUtility
 from zope.schema.interfaces import IVocabularyFactory
 from Products.Five import BrowserView
@@ -11,22 +12,28 @@ class AutocompleteView(BrowserView):
     def __call__(self):
         self.request.response.setHeader("Content-type", "application/json")
 
-        source_name = self.request.get('source', None)
-        if not source_name:
+        factory_name = self.request.get('source', None)
+        if not factory_name:
             return json.dumps({'error': 'No source provided'})
 
-        source = queryUtility(IVocabularyFactory, source_name)
-        if not source:
+        factory = queryUtility(IVocabularyFactory, factory_name)
+        if not factory:
             return json.dumps({
-                'error': 'No source with name "%s".' % source_name})
+                'error': 'No source with name "%s".' % factory_name})
 
-        source = source(self.context)
+        # check if factory excepts query argument
+        query = self.request.get('query', '')
+        factory_spec = inspect.getargspec(factory.__call__)
+        if query and len(factory_spec.args) >= 3 and \
+                factory_spec.args[2] == 'query':
+            vocabulary = factory(self.context, query)
+        else:
+            vocabulary = factory(self.context)
 
         items = []
-        for item in source:
+        for item in vocabulary:
             items.append(item.token)
 
-        # TODO: add option for querying
         # TODO: add option for limiting number of results
         # TODO: add option for batching
         # TODO: add option for sorting
