5c87dfadda087a026cd9124c7fd47aa6c135d50a
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-04-05 17:43:08 +0300

    Added source code viewer and restructured text descriptions

---
+++ plone/app/widgets/demos/source.py
@@ -0,0 +1,24 @@
+"""
+
+    Helpers to show source code examples.
+
+"""
+
+import os
+import sys
+
+
+def get_class_source(klass):
+    """ Grab the source code of the module declaring a Python class.
+    """
+
+    mod = klass.__module__
+    path = sys.modules[mod].__file__
+
+    path = path.replace(".pyc", ".py")
+
+    f = open(path, "rt")
+    text = f.read()
+    f.close()
+    return text
+
