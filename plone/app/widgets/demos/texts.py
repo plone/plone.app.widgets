1b1378eddd177d4ba4de7eab291bb0c6e770177f
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-04-05 17:49:00 +0300

    Added text string examples

---
+++ plone/app/widgets/demos/texts.py
@@ -0,0 +1,38 @@
+"""
+
+    Demo form for z3c.form fields and widgets
+
+"""
+
+from zope.interface import Interface
+import zope.schema
+
+from plone.supermodel import model
+
+from plone.app.widgets.demos.demo import widget_demo
+from plone.app.widgets.demos.demo import WidgetDemoForm
+from plone.app.widgets.demos.demo import IN_CORE_DESCRIPTION
+
+
+class ITextExamples(model.Schema):
+    """ Single choice and multiple choice examples """
+
+    textLine = zope.schema.TextLine(
+        title=u"zope.schema.TextLine",
+        description=u"Unicode string")
+
+    asciiLine = zope.schema.TextLine(
+        title=u"zope.schema.ASCIILine",
+        description=u"7-bit text string (bytes)")
+
+
+@widget_demo
+class TextExamples(WidgetDemoForm):
+    """
+    """
+    schema = ITextExamples
+
+    label = u"Text examples"
+
+    package = IN_CORE_DESCRIPTION  # Says that shipped with vanilla Plone
+    layer = Interface  # Means that eanbled withou taddons (all browserlayers)
