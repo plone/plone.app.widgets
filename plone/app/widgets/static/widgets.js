3b718c79b3fa4c137c55b7d00f3815c60c4f9a64
Rok Garbas <rok@garbas.si>
2013-04-30 16:43:47 +0200

    update widgets bundle

--- plone/app/widgets/static/widgets.js
+++ plone/app/widgets/static/widgets.js
@@ -7,11 +7,6 @@
     var script = document.createElement('script');
     script.setAttribute('type', 'text/javascript');
     script.setAttribute('src', '++resource++mockup/jam/require.js');
-    //requirejs.onreadystatechange = function() {
-    //  if (requirejs.readyState == "loaded") {
-    //    window.require(['++resource++mockup/js/bundles/widgets.js']);
-    //  }
-    //};
     script.onload = function() {
       requirejs.config({ baseUrl: '++resource++mockup/' });
       window.require(['js/bundles/widgets'], function(Widgets) {
