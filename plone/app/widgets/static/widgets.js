576563c0ec26aac2af5379fb6899c16e20d63436
Rok Garbas <rok@garbas.si>
2013-07-15 17:10:42 +0200

    updating widgets.js

--- plone/app/widgets/static/widgets.js
+++ plone/app/widgets/static/widgets.js
@@ -1,19 +1,24 @@
-
 (function($) {
   "use strict";
 
   $(document).ready(function() {
 
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', '++resource++mockup/jam/require.js');
-    script.onload = function() {
-      requirejs.config({ baseUrl: '++resource++mockup/' });
-      window.require(['js/bundles/widgets'], function(Widgets) {
-        Widgets.scan('body');
-      });
+    var script1 = document.createElement('script');
+    script1.setAttribute('type', 'text/javascript');
+    script1.setAttribute('src', '/++resource++mockup/bower_components/requirejs/require.js');
+    script1.onload = function() {
+      var script2 = document.createElement('script');
+      script2.setAttribute('type', 'text/javascript');
+      script2.setAttribute('src', '/++resource++mockup/js/config.js');
+      script1.onload = function() {
+        requirejs.config({ baseUrl: '++resource++mockup/' });
+        window.require(['mockup-bundles-widgets'], function(Toolbar) {
+          Toolbar.scan('body');
+        });
+      };
+      document.getElementsByTagName("head")[0].appendChild(script2);
     };
-    document.getElementsByTagName("head")[0].appendChild(script);
+    document.getElementsByTagName("head")[0].appendChild(script1);
 
   });
 
