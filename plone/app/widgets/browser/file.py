43195ff4e7b12c6a270107b36e94dc5c7d1df973
Nathan Van Gheem <vangheem@gmail.com>
2013-09-24 15:03:48 -0500

    move browser.py into folder

---
+++ plone/app/widgets/browser/file.py
@@ -0,0 +1,80 @@
+
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+
+import json
+import pkg_resources
+import mimetypes
+try:
+    pkg_resources.get_distribution('plone.dexterity')
+except pkg_resources.DistributionNotFound:
+    HAS_DEXTERITY = False
+else:
+    from plone.dexterity.interfaces import IDexterityFTI
+    HAS_DEXTERITY = True
+from plone.app.widgets.interfaces import IATCTFileFactory, IDXFileFactory
+from plone.uuid.interfaces import IUUID
+
+
+class FileUploadView(BrowserView):
+
+    def __call__(self):
+        req = self.request
+        if req.REQUEST_METHOD != 'POST':
+            return
+        filedata = self.request.form.get("file", None)
+        if filedata is None:
+            return
+        filename = filedata.filename
+        content_type = mimetypes.guess_type(filename)[0] or ""
+
+        if not filedata:
+            return
+
+        # Determine if the default file/image types are DX or AT based
+        ctr = getToolByName(self.context, 'content_type_registry')
+        type_ = ctr.findTypeName(filename.lower(), '', '') or 'File'
+
+        DX_BASED = False
+        if HAS_DEXTERITY:
+            pt = getToolByName(self.context, 'portal_types')
+            if IDexterityFTI.providedBy(getattr(pt, type_)):
+                factory = IDXFileFactory(self.context)
+                DX_BASED = True
+            else:
+                factory = IATCTFileFactory(self.context)
+        else:
+            factory = IATCTFileFactory(self.context)
+
+        obj = factory(filename, content_type, filedata)
+
+        if DX_BASED:
+            if 'File' in obj.portal_type:
+                size = obj.file.getSize()
+                content_type = obj.file.contentType
+            elif 'Image' in obj.portal_type:
+                size = obj.image.getSize()
+                content_type = obj.image.contentType
+
+            result = {
+                "type": content_type,
+                "size": size
+            }
+        else:
+            try:
+                size = obj.getSize()
+            except AttributeError:
+                size = obj.getObjSize()
+
+            result = {
+                "type": obj.getContentType(),
+                "size": size
+            }
+
+        result.update({
+            'url': obj.absolute_url(),
+            'name': obj.getId(),
+            'uid': IUUID(obj),
+            'filename': filename
+        })
+        return json.dumps(result)
--- plone/app/widgets/browser/vocabulary.py
+++ plone/app/widgets/browser/vocabulary.py
@@ -2,7 +2,6 @@ from AccessControl import getSecurityManager
 from AccessControl import Unauthorized
 from logging import getLogger
 from plone.app.vocabularies.interfaces import ISlicableVocabulary
-from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 from Products.ZCTextIndex.ParseTree import ParseError
 from types import FunctionType
@@ -10,19 +9,6 @@ from zope.component import queryUtility
 from zope.schema.interfaces import IVocabularyFactory
 import inspect
 import json
-import mimetypes
-
-import pkg_resources
-
-try:
-    pkg_resources.get_distribution('plone.dexterity')
-except pkg_resources.DistributionNotFound:
-    HAS_DEXTERITY = False
-else:
-    from plone.dexterity.interfaces import IDexterityFTI
-    HAS_DEXTERITY = True
-from plone.app.widgets.interfaces import IATCTFileFactory, IDXFileFactory
-from plone.uuid.interfaces import IUUID
 
 logger = getLogger(__name__)
 
@@ -46,70 +32,6 @@ def _parseJSON(s):
 
 _unsafe_metadata = ['Creator', 'listCreators']
 _safe_callable_metadata = ['getURL', 'getPath']
-
-
-class FileUploadView(BrowserView):
-
-    def __call__(self):
-        req = self.request
-        if req.REQUEST_METHOD != 'POST':
-            return
-        filedata = self.request.form.get("file", None)
-        if filedata is None:
-            return
-        filename = filedata.filename
-        content_type = mimetypes.guess_type(filename)[0] or ""
-
-        if not filedata:
-            return
-
-        # Determine if the default file/image types are DX or AT based
-        ctr = getToolByName(self.context, 'content_type_registry')
-        type_ = ctr.findTypeName(filename.lower(), '', '') or 'File'
-
-        DX_BASED = False
-        if HAS_DEXTERITY:
-            pt = getToolByName(self.context, 'portal_types')
-            if IDexterityFTI.providedBy(getattr(pt, type_)):
-                factory = IDXFileFactory(self.context)
-                DX_BASED = True
-            else:
-                factory = IATCTFileFactory(self.context)
-        else:
-            factory = IATCTFileFactory(self.context)
-
-        obj = factory(filename, content_type, filedata)
-
-        if DX_BASED:
-            if 'File' in obj.portal_type:
-                size = obj.file.getSize()
-                content_type = obj.file.contentType
-            elif 'Image' in obj.portal_type:
-                size = obj.image.getSize()
-                content_type = obj.image.contentType
-
-            result = {
-                "type": content_type,
-                "size": size
-            }
-        else:
-            try:
-                size = obj.getSize()
-            except AttributeError:
-                size = obj.getObjSize()
-
-            result = {
-                "type": obj.getContentType(),
-                "size": size
-            }
-
-        result.update({
-            'url': obj.absolute_url(),
-            'name': obj.getId(),
-            'uid': IUUID(obj),
-            'filename': filename
-        })
-        return json.dumps(result)
 
 
 class VocabularyView(BrowserView):
