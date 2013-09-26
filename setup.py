5e9ab61ae552bc35512932024d677787ba0a409d
Nathan Van Gheem <vangheem@gmail.com>
2013-09-26 15:47:25 -0500

    require path depth querystring compatible version

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ setup(
         'plone.app.jquery>=1.8.0',
         # needed so it works with jquery >= 1.8
         'plone.app.search>=1.1.2',
-        'plone.app.registry>=1.2.2'
+        'plone.app.registry>=1.2.2',
+        'plone.app.querystring>=1.1.0'
     ],
     extras_require={
         'test': [
