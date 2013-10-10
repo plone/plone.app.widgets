39d6707589e64b701450ab25e3b0e08848d474ff
Rok Garbas <rok@garbas.si>
2013-10-10 16:05:30 +0200

    docs for dexterity widgets

--- plone/app/widgets/dx.py
+++ plone/app/widgets/dx.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from Products.CMFCore.interfaces import ISiteRoot
 from Products.CMFCore.utils import getToolByName
 from datetime import date
 from datetime import datetime
-from plone.app.layout.navigation.root import getNavigationRootObject
 from plone.app.widgets import base
 from plone.app.widgets import utils
 from z3c.form import interfaces as z3cform_interfaces
 from z3c.form.browser.select import SelectWidget as z3cform_SelectWidget
 from z3c.form.converter import BaseDataConverter
-from z3c.form.interfaces import NO_VALUE
 from z3c.form.widget import Widget
 from zope.component import adapts
-from zope.component import providedBy
 from zope.component import queryUtility
-from zope.component.hooks import getSite
-from zope.i18n import translate
-from zope.i18nmessageid import MessageFactory
 from zope.interface import implementsOnly
 from zope.schema.interfaces import ICollection
 from zope.schema.interfaces import IDate
@@ -27,12 +20,9 @@ from zope.schema.interfaces import IVocabularyFactory
 
 import json
 
-_ = MessageFactory('plone.app.widgets')
-_plone = MessageFactory('plone')
-
 
 class NotImplemented(Exception):
-    pass
+    """Raised when method/property is not implemented"""
 
 
 class IDatetimeWidget(z3cform_interfaces.ITextWidget):
@@ -60,12 +50,12 @@ class IRelatedItemsWidget(z3cform_interfaces.ITextWidget):
 
 
 class DatetimeWidgetConverter(BaseDataConverter):
-    """Data converter for datetime stuff."""
+    """Data converter for datetime fields."""
 
     adapts(IDatetime, IDatetimeWidget)
 
     def toWidgetValue(self, value):
-        """Coverts from field value to widget.
+        """Converts from field value to widget.
 
         :param value: Field value.
         :type value: datetime
@@ -82,7 +72,7 @@ class DatetimeWidgetConverter(BaseDataConverter):
         """Converts from widget value to field.
 
         :param value: Value inserted by datetime widget.
-        :param value: string
+        :type value: string
 
         :returns: `datetime.datetime` object.
         :rtype: datetime
@@ -98,34 +88,66 @@ class DatetimeWidgetConverter(BaseDataConverter):
 
 
 class DateWidgetConverter(BaseDataConverter):
-    """Data converter for date stuff.
-    """
+    """Data converter for date fields."""
 
     adapts(IDate, IDateWidget)
 
     def toWidgetValue(self, value):
+        """Converts from field value to widget.
+
+        :param value: Field value.
+        :type value: date
+
+        :returns: Date in format `Y-m-d`
+        :rtype: string
+        """
         if value is self.field.missing_value:
             return u''
-        return '%d-%02d-%02d' % (value.year, value.month, value.day)
+        return ('{value.year:}-{value.month:02}-{value.day:02}'
+                ).format(value=value)
 
     def toFieldValue(self, value):
+        """Converts from widget value to field.
+
+        :param value: Value inserted by date widget.
+        :type value: string
+
+        :returns: `date.date` object.
+        :rtype: date
+        """
         if not value:
             return self.field.missing_value
         return date(*map(int, value.split('-')))
 
 
 class AjaxSelectWidgetConverter(BaseDataConverter):
-    """Data converter for ICollection.
-    """
+    """Data converter for ICollection."""
 
     adapts(ICollection, IAjaxSelectWidget)
 
     def toWidgetValue(self, value):
+        """Converts from field value to widget.
+
+        :param value: Field value.
+        :type value: list |tuple | set
+
+        :returns: Items separated using separator defined on widget
+        :rtype: string
+        """
         if not value:
-            return u''
-        return self.widget.separator.join(unicode(v) for v in value)
+            return self.field.missing_value
+        separator = getattr(self.widget, 'separator', ';')
+        return separator.join(unicode(v) for v in value)
 
     def toFieldValue(self, value):
+        """Converts from widget value to field.
+
+        :param value: Value inserted by AjaxSelect widget.
+        :type value: string
+
+        :returns: List of items
+        :rtype: list | tuple | set
+        """
         collectionType = self.field._type
         if isinstance(collectionType, tuple):
             collectionType = collectionType[-1]
@@ -134,59 +156,104 @@ class AjaxSelectWidgetConverter(BaseDataConverter):
         valueType = self.field.value_type._type
         if isinstance(valueType, tuple):
             valueType = valueType[0]
+        separator = getattr(self.widget, 'separator', ';')
         return collectionType(valueType and valueType(v) or v
-                              for v in value.split(self.widget.separator))
+                              for v in value.split(separator))
 
 
 class RelatedItemsDataConverter(BaseDataConverter):
-    """Data converter for ICollection.
-    """
+    """Data converter for ICollection."""
 
     adapts(ICollection, IRelatedItemsWidget)
 
     def toWidgetValue(self, value):
+        """Converts from field value to widget.
+
+        :param value: List of catalog brains.
+        :type value: list
+
+        :returns: List of of UID separated by separator defined on widget.
+        :rtype: string
+        """
         if not value:
-            return u''
-        return self.widget.separator.join(unicode(v.UID()) for v in value)
+            return self.field.missing_value
+        separator = getattr(self.widget, 'separator', ';')
+        return separator.join(unicode(v.UID()) for v in value)
 
     def toFieldValue(self, value):
+        """Converts from widget value to field.
+
+        :param value: List of UID's separated by separator defined
+        :type value: string
+
+        :returns: List of content objects
+        :rtype: list | tuple | set
+        """
         collectionType = self.field._type
         if isinstance(collectionType, tuple):
             collectionType = collectionType[-1]
         if not len(value):
             return self.field.missing_value
+
         catalog = getToolByName(self.widget.context, 'portal_catalog')
-        value = value.split(self.widget.separator)
+        separator = getattr(self.widget, 'separator', ';')
+        value = value.split(separator)
         value = [v.split('/')[0] for v in value]
         results = catalog(UID=value)
         return collectionType(item.getObject() for item in results)
 
 
 class QueryStringDataConverter(BaseDataConverter):
-    """data converter for ilist.
-    """
+    """Data converter for IList."""
 
     adapts(IList, IQueryStringWidget)
 
     def toWidgetValue(self, value):
+        """Converts from field value to widget.
+
+        :param value: Query string.
+        :type value: list
+
+        :returns: Query string converted to JSON.
+        :rtype: string
+        """
         if value is self.field.missing_value:
-            return u''
+            return self.field.missing_value
         return json.dumps(value)
 
     def toFieldValue(self, value):
+        """Converts from widget value to field.
+
+        :param value: Query string.
+        :type value: string
+
+        :returns: Query string.
+        :rtype: list
+        """
         if value is self.field.missing_value:
             return self.field.missing_value
         return json.loads(value)
 
 
 class BaseWidget(Widget):
-    """Base pattern widget for z3c.form."""
+    """Base widget for z3c.form."""
 
     pattern = None
     pattern_options = {}
 
+    def _base(self):
+        """Base widget class."""
+        raise NotImplemented
+
     def _base_args(self):
-        """
+        """Method which will calculate _base class arguments.
+
+        Returns (as python dictionary):
+            - `pattern`: pattern name
+            - `pattern_options`: pattern options
+
+        :returns: Arguments which will be passed to _base
+        :rtype: dict
         """
         if self.pattern is None:
             raise NotImplemented("'pattern' options not provided.")
@@ -196,8 +263,9 @@ class BaseWidget(Widget):
         }
 
     def render(self):
-        """
-        :returns:
+        """Render widget.
+
+        :returns: Widget's HTML.
         :rtype: string
         """
         if self.mode != 'input':
@@ -206,7 +274,7 @@ class BaseWidget(Widget):
 
 
 class DatetimeWidget(BaseWidget):
-    """Datetime pattern widget for z3c.form."""
+    """Datetime widget for z3c.form."""
 
     _base = base.InputWidget
 
@@ -216,54 +284,56 @@ class DatetimeWidget(BaseWidget):
     pattern_options = BaseWidget.pattern_options.copy()
 
     def _base_args(self):
+        """Method which will calculate _base class arguments.
+
+        Returns (as python dictionary):
+            - `pattern`: pattern name
+            - `pattern_options`: pattern options
+            - `name`: field name
+            - `value`: field value
+            - `date`: options for pickdate widget
+            - `time`: options for picktime widget
+
+        :returns: Arguments which will be passed to _base
+        :rtype: dict
+        """
         args = super(DatetimeWidget, self)._base_args()
         args['name'] = self.name
 
         value = (self.request.get(self.name, self.value) or u'').strip()
-        if len(value.split(' ')) == 1:
+        if value and len(value.split(' ')) == 1:
             value += ' 00:00'
         args['value'] = value
 
-        today = date.today()
         args.setdefault('pattern_options', {})
-        _pattern_options_calendar = utils.get_calendar_options(self.request)
-        _pattern_options_defaults = {
-            'selectYears': 200,
-            'min': [today.year - 100, 1, 1],
-            'max': [today.year + 20, 1, 1],
-            'format_date': translate(
-                _('pickadate_date_format', default='mmmm d, yyyy'),
-                context=self.request),
-            'format_time': translate(
-                _('pickadate_time_format', default='h:i a'),
-                context=self.request),
-            'placeholderDate': translate(_plone('Enter date...'),
-                                         context=self.request),
-            'placeholderTime': translate(_plone('Enter time...'),
-                                         context=self.request),
-            'today': translate(_plone(u"Today"), context=self.request),
-            'clear': translate(_plone(u"Clear"), context=self.request),
-        }
 
-        args['pattern_options'] = base.dict_merge(
-            args['pattern_options'],
-            _pattern_options_defaults)
+        args['pattern_options'].setdefault('date', {})
+        args['pattern_options']['date'] = base.dict_merge(
+            args['pattern_options']['date'],
+            utils.get_date_options(self.request))
 
-        args['pattern_options'] = base.dict_merge(
-            args['pattern_options'],
-            _pattern_options_calendar)
+        args['pattern_options'].setdefault('time', {})
+        args['pattern_options']['time'] = base.dict_merge(
+            args['pattern_options']['time'],
+            utils.get_time_options(self.request))
 
         return args
 
     def render(self):
+        """Render widget.
+
+        :returns: Widget's HTML.
+        :rtype: string
+        """
         if self.mode != 'display':
             return super(DatetimeWidget, self).render()
 
         if not self.value:
-            return ''
+            return u''
 
         field_value = DatetimeWidgetConverter(
             self.field, self).toFieldValue(self.value)
+
         if field_value is self.fields.missing_value:
             return u''
 
@@ -277,18 +347,31 @@ class DatetimeWidget(BaseWidget):
 
 
 class DateWidget(DatetimeWidget):
+    """Date widget for z3c.form."""
 
     implementsOnly(IDateWidget)
 
     pattern_options = DatetimeWidget.pattern_options.copy()
 
-    def _widget_args(self):
+    def _base_args(self):
+        """Method which will calculate _base class arguments.
+
+        Returns (as python dictionary):
+            - `pattern`: pattern name
+            - `pattern_options`: pattern options
+            - `name`: field name
+            - `value`: field value
+
+        :returns: Arguments which will be passed to _base
+        :rtype: dict
+        """
         args = super(DateWidget, self)._base_args()
         args['pattern_options']['time'] = False
-        del args['pattern_options']['format_time']
         return args
 
     def render(self):
+        """
+        """
         if self.mode != 'display':
             return super(DateWidget, self).render()
 
@@ -310,29 +393,45 @@ class DateWidget(DatetimeWidget):
 
 
 class SelectWidget(BaseWidget, z3cform_SelectWidget):
+    """Select widget for z3c.form."""
 
-    _base_klass = base.SelectWidget
+    _base = base.SelectWidget
 
     implementsOnly(ISelectWidget)
 
     pattern = 'select2'
     pattern_options = BaseWidget.pattern_options.copy()
+    multiple = False
 
     def _base_args(self):
-        args = super(SelectWidget, self)._base_args()
+        """Method which will calculate _base class arguments.
 
-        options = []
+        Returns (as python dictionary):
+            - `pattern`: pattern name
+            - `pattern_options`: pattern options
+            - `name`: field name
+            - `value`: field value
+            - `multiple`: field multiple
+            - `items`: field items from which we can select to
+
+        :returns: Arguments which will be passed to _base
+        :rtype: dict
+        """
+        args = super(SelectWidget, self)._base_args()
+        args['name'] = self.name
+        args['value'] = self.value
+        args['multiple'] = self.multiple
+
+        items = []
         for item in self.items():
-            options.append((item['value'], item['content']))
-        args['options'] = options
-        args['selected'] = self.value
-        if self.multiple:
-            args['multiple'] = 'multiple'
+            items.append((item['value'], item['content']))
+        args['items'] = items
 
         return args
 
 
 class AjaxSelectWidget(BaseWidget):
+    """Ajax select widget for z3c.form."""
 
     _base = base.InputWidget
 
@@ -345,23 +444,21 @@ class AjaxSelectWidget(BaseWidget):
     vocabulary = None
 
     def _base_args(self):
+        """Method which will calculate _base class arguments.
 
-        def get_portal():
-            closest_site = getSite()
-            if closest_site is not None:
-                for potential_portal in closest_site.aq_chain:
-                    if ISiteRoot in providedBy(potential_portal):
-                        return potential_portal
+        Returns (as python dictionary):
+            - `pattern`: pattern name
+            - `pattern_options`: pattern options
+            - `name`: field name
+            - `value`: field value
 
-        def get_portal_url():
-            portal = get_portal()
-            if portal:
-                root = getNavigationRootObject(self.context, portal)
-                if root:
-                    return root.absolute_url()
-            return ''
+        :returns: Arguments which will be passed to _base
+        :rtype: dict
+        """
 
         args = super(AjaxSelectWidget, self)._base_args()
+        args['name'] = self.name
+        args['value'] = self.value
         args['pattern_options']['separator'] = self.separator
 
         vocabulary_factory = getattr(self.field, 'vocabulary_factory', None)
@@ -371,7 +468,7 @@ class AjaxSelectWidget(BaseWidget):
         # get url which will be used to lookup vocabulary
         if self.vocabulary:
             vocabulary_url = '%s/@@getVocabulary?name=%s' % (
-                get_portal_url(), self.vocabulary)
+                utils.get_portal_url(self.context), self.vocabulary)
             args['pattern_options']['vocabularyUrl'] = vocabulary_url
 
             # initial values
@@ -394,24 +491,37 @@ class AjaxSelectWidget(BaseWidget):
 
 
 class QueryStringWidget(BaseWidget):
+    """QueryString widget for z3c.form."""
+
+    _base = base.InputWidget
+
+    implementsOnly(IQueryStringWidget)
 
     pattern = 'querystring'
     pattern_options = BaseWidget.pattern_options.copy()
 
-    implementsOnly(IQueryStringWidget)
-
     def _base_args(self):
-        args = super(QueryStringWidget, self)._widget_args()
+        """Method which will calculate _base class arguments.
 
-        if 'pattern_options' not in args:
-            args['pattern_options'] = {}
+        Returns (as python dictionary):
+            - `pattern`: pattern name
+            - `pattern_options`: pattern options
+            - `name`: field name
+            - `value`: field value
+
+        :returns: Arguments which will be passed to _base
+        :rtype: dict
+        """
+        args = super(QueryStringWidget, self)._base_args()
+        args['name'] = self.name
+        args['value'] = self.value
         args['pattern_options']['indexOptionsUrl'] = '%s/@@qsOptions' % (
-            getSite().absolute_url())
-
+            utils.get_portal_url(self.context))
         return args
 
 
 class RelatedItemsWidget(AjaxSelectWidget):
+    """RelatedItems widget for z3c.form."""
 
     pattern = 'relateditems'
     pattern_options = AjaxSelectWidget.pattern_options.copy()
