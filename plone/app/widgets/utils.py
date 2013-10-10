f78d78a9bc73d45b5ab5b89c456cf7590f1012e7
Rok Garbas <rok@garbas.si>
2013-10-10 16:02:19 +0200

    more utils

--- plone/app/widgets/utils.py
+++ plone/app/widgets/utils.py
@@ -1,9 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from Products.CMFCore.interfaces import ISiteRoot
+from datetime import datetime
+from plone.app.layout.navigation.root import getNavigationRootObject
+from zope.component import providedBy
+from zope.component.hooks import getSite
+from zope.i18n import translate
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('plone.app.widgets')
+_plone = MessageFactory('plone')
 
 
-def get_calendar_options(request):
+def get_date_options(request):
     calendar = request.locale.dates.calendars['gregorian']
     # TODO: take first weekday into account when you like
     # weekdaysFull, weekdaysShort
+    today = datetime.today()
     return {
         'weekdaysFull': [
             calendar.days.get(t, (None, None))[0]
@@ -13,7 +26,43 @@ def get_calendar_options(request):
             for t in (7, 1, 2, 3, 4, 5, 6)],
         'monthsFull': calendar.getMonthNames(),
         'monthsShort': calendar.getMonthAbbreviations(),
+        'selectYears': 200,
+        'min': [today.year - 100, 1, 1],
+        'max': [today.year + 20, 1, 1],
+        'format': translate(
+            _('pickadate_date_format', default='mmmm d, yyyy'),
+            context=request),
+        'placeholder': translate(_plone('Enter date...'), context=request),
+        'today': translate(_plone(u"Today"), context=request),
+        'clear': translate(_plone(u"Clear"), context=request),
     }
+
+
+def get_time_options(request):
+    return {
+        'format': translate(
+            _('pickadate_time_format', default='h:i a'),
+            context=request),
+        'placeholder': translate(_plone('Enter time...'), context=request),
+        'today': translate(_plone(u"Today"), context=request),
+    }
+
+
+def get_portal():
+    closest_site = getSite()
+    if closest_site is not None:
+        for potential_portal in closest_site.aq_chain:
+            if ISiteRoot in providedBy(potential_portal):
+                return potential_portal
+
+
+def get_portal_url(context):
+    portal = get_portal()
+    if portal:
+        root = getNavigationRootObject(context, portal)
+        if root:
+            return root.absolute_url()
+    return ''
 
 #from zope.component import queryMultiAdapter
 #from zope.i18nmessageid import MessageFactory
