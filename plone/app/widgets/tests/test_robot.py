2bf366dae62cd2110944733cfd9d46ac30b67b00
David Erni <david.erni@4teamwork.ch>
2013-10-13 11:24:37 -0300

    Add robot test suite setup.

---
+++ plone/app/widgets/tests/test_robot.py
@@ -0,0 +1,28 @@
+from plone.app.widgets.testing import PLONEAPPWIDGETS_DX_ROBOT_TESTING
+from plone.testing import layered
+import os
+import robotsuite
+import unittest
+
+
+ROBOT_TEST_LEVEL = 5
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    robot_dir = os.path.join(current_dir, 'robot')
+    robot_tests = [
+        os.path.join('robot', doc) for doc in
+        os.listdir(robot_dir) if doc.endswith('.robot') and
+        doc.startswith('test_')
+    ]
+    for robot_test in robot_tests:
+        robottestsuite = robotsuite.RobotTestSuite(robot_test)
+        robottestsuite.level = ROBOT_TEST_LEVEL
+        suite.addTests([
+            layered(robottestsuite,
+                    layer=PLONEAPPWIDGETS_DX_ROBOT_TESTING
+            ),
+        ])
+    return suite
