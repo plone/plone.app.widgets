b411feb3f0619032ccac2d7d9f054eba8cb4bf4b
Rok Garbas <rok@garbas.si>
2013-10-18 23:56:16 +0200

    removing demo stuff since we'll do it with sphinx

--- plone/app/widgets/demos/interfaces.py
+++
@@ -1,9 +0,0 @@
-from zope.interface import Interface
-
-
-class IWidgetDemo(Interface):
-    """ A marker interface marking form for widgets demo.
-
-    Demo form dynamically polls all the demo adapter registrations and
-    fills the demo fields with these.
-    """
