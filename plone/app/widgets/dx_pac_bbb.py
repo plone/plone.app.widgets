ea6cac0c2f6e7e14273983388fa63e01f356b2d6
Asko Soukka <asko.soukka@iki.fi>
2013-11-14 23:28:14 +0100

    Fix ICollection import from type interface to behavior interface

--- plone/app/widgets/dx_pac_bbb.py
+++ plone/app/widgets/dx_pac_bbb.py
@@ -3,7 +3,7 @@ from z3c.form.interfaces import IFieldWidget
 from z3c.form.util import getSpecification
 from zope.interface import implementer
 from zope.component import adapter
-from plone.app.contenttypes.interfaces import ICollection
+from plone.app.contenttypes.behaviors.collection import ICollection
 from plone.app.event.dx.behaviors import IEventBasic
 from plone.app.widgets.dx import QueryStringWidget
 from plone.app.widgets.dx import DatetimeWidget
