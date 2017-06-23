plone/plone.app.widgets

commit 5c75aa8d2a1637b80b461200afd5b2b51fd74f88

Unset and reset ``define`` and ``require`` Unset and reset ``define`` and ``require`` before and after the plone.app.widgets JavaScript bundle. This avoids the infamous ``Mismatched anonymous define`` error with unbundled JavaScript supporting RequireJS. See: http://requirejs.org/docs/errors.html#mismatch

plone/app/widgets/static/requirejs-reset.js
@@ -1,0 +1,2 @@
+var define = _old_define;
+var require = _old_require;

plone/app/widgets/static/requirejs-unset.js
@@ -1,0 +1,4 @@
+var _old_define = define;
+var _old_require = require;
+define = undefined;
+require = undefined;

plone/app/widgets/configure.zcml
@@ -16,6 +16,17 @@
   <include package=".browser" />
 
   <browser:resource
+      name="plone.app.widgets-requirejs-unset.js"
+      file="static/requirejs-unset.js"
+      layer=".interfaces.IWidgetsLayer"
+      />
+  <browser:resource
+      name="plone.app.widgets-requirejs-reset.js"
+      file="static/requirejs-reset.js"
+      layer=".interfaces.IWidgetsLayer"
+      />
+
+  <browser:resource
       name="plone.app.widgets-bootstrap-glyphicons-halflings-regular.eot"
       file="static/widgets-bootstrap-glyphicons-halflings-regular.eot"
       layer=".interfaces.IWidgetsLayer"

plone/app/widgets/profiles/default/jsregistry.xml
@@ -25,4 +25,24 @@
         inline="False"
         />
 
+    <javascript
+        id="++resource++plone.app.widgets-requirejs-unset.js"
+        insert-before="++resource++plone.app.widgets.js"
+        cacheable="True"
+        compression="safe"
+        cookable="True"
+        enabled="True"
+        inline="False"
+        />
+
+    <javascript
+        id="++resource++plone.app.widgets-requirejs-reset.js"
+        insert-after="++resource++plone.app.widgets.js"
+        cacheable="True"
+        compression="safe"
+        cookable="True"
+        enabled="True"
+        inline="False"
+        />
+
 </object>

plone/app/widgets/profiles/uninstall/jsregistry.xml
@@ -15,9 +15,19 @@
     <javascript bundle="default" id="tiny_mce_gzip.js" />
     <javascript bundle="default" id="toc.js" />
 
-    <javascript remove="True"
+    <javascript
+        remove="True"
         id="++resource++plone.app.widgets.js"
         />
 
-</object>
+    <javascript
+        remove="True"
+        id="++resource++plone.app.widgets-requirejs-unset.js"
+        />
 
+    <javascript
+        remove="True"
+        id="++resource++plone.app.widgets-requirejs-reset.js"
+        />
+
+</object>

commit 18bc1fe295e2af27e7b2f6ac16e88c1c2cf0d14d

Failsafe requirejs unset/reset
Introduced in 5c75aa8d2a1637b80b461200afd5b2b51fd74f88 it might happen,
that there is no `define` or `require` defined. handle this gracefully.

plone/app/widgets/static/requirejs-reset.js
@@ -1,2 +1,2 @@
-var define = _old_define;
-var require = _old_require;
+if(typeof _old_define !== 'undefined') { var define = _old_define; }
+if(typeof _old_require !== 'undefined') { var require = _old_require; }

plone/app/widgets/static/requirejs-unset.js
@@ -1,4 +1,4 @@
-var _old_define = define;
-var _old_require = require;
+if(typeof define !== 'undefined') { var _old_define = define; }
+if(typeof require !== 'undefined') { var _old_require = require; }
 define = undefined;
 require = undefined;
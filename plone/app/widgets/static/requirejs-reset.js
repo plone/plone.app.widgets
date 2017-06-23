18bc1fe295e2af27e7b2f6ac16e88c1c2cf0d14d
Peter Mathis <peter.mathis@kombinat.at>
2017-06-23 08:38:35 +0200

    Failsafe requirejs unset/reset
    Introduced in 5c75aa8d2a1637b80b461200afd5b2b51fd74f88 it might happen,
    that there is no `define` or `require` defined. handle this gracefully.

--- plone/app/widgets/static/requirejs-reset.js
+++ plone/app/widgets/static/requirejs-reset.js
@@ -1,2 +1,2 @@
-var define = _old_define;
-var require = _old_require;
+if(typeof _old_define !== 'undefined') { var define = _old_define; }
+if(typeof _old_require !== 'undefined') { var require = _old_require; }
