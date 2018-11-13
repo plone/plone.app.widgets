ee0b226180c55590d3470de9ecc6908389f984f1
Gil Forcada <gil.gnome@gmail.com>
2018-11-13 13:03:09 +0100

    Branch 2.4.x targets only Plone 5.1

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 5.1",
-        "Framework :: Plone :: 5.2",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
