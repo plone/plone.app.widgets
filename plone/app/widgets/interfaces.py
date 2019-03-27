1833a6bbf8f2170de73f5d90428db0564a8e2b4b
Jens W. Klein <jk@kleinundpartner.at>
2019-03-27 10:02:04 +0100

    show that those two interfaces are deprecated

--- plone/app/widgets/interfaces.py
+++ plone/app/widgets/interfaces.py
@@ -1,15 +1,18 @@
 from zope.interface import Interface
 from plone.app.z3cform.interfaces import IPloneFormLayer
-from zope.filerepresentation.interfaces import IFileFactory
 
 
 class IWidgetsLayer(IPloneFormLayer):
     """Browser layer used to indicate that plone.app.widgets is installed
+
+    DEPRECATED
     """
 
 
 class IWidgetsView(Interface):
     """A view that gives access to various widget related functions.
+
+    DEPRECATED
     """
 
     def getVocabulary():
