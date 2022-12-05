9c0cd7544da1c726f547b6822ef5410f1661e3a1
Maurits van Rees <maurits@vanrees.org>
2022-12-05 10:13:21 +0100

    Add python_requires
    [ci skip]

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     namespace_packages=['plone', 'plone.app'],
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=3.8',
     install_requires=[
         'setuptools',
         # needed because users vocabulary was added here
